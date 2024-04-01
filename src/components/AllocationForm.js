394e52d91b2a2ac71a3e99d199d8d8b311dc6abf
Rupam Kuehner <rupamkuehner@yahoo.com>
2024-04-01 17:28:28 -0400

    Adding a currency label to the amount input field

--- src/components/AllocationForm.js
+++ src/components/AllocationForm.js
@@ -8,6 +8,8 @@ const AllocationForm = (props) => {
     const [cost, setCost] = useState('');
     const [action, setAction] = useState('');
 
+    const currency = "£"
+    
     const submitEvent = () => {
 
             if(cost > remaining) {
@@ -58,13 +60,15 @@ const AllocationForm = (props) => {
                         <option defaultValue value="Add" name="Add">Add</option>
                 <option value="Reduce" name="Reduce">Reduce</option>
                   </select>
-
+                  <div className="input-group-prepend" style={{ marginLeft: '2rem' }}>
+                  <label className="input-group-text" htmlFor="cost">£</label>
+                    </div>
                     <input
                         required='required'
                         type='number'
                         id='cost'
                         value={cost}
-                        style={{ marginLeft: '2rem' , size: 10}}
+                        style={{ size: 10}}
                         onChange={(event) => setCost(event.target.value)}>
                         </input>
 
