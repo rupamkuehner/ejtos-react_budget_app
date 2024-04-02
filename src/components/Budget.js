12eafd0590fe57561b7f1ffd6f18074c508f620e
Rupam Kuehner <rupamkuehner@yahoo.com>
2024-04-02 17:27:21 -0400

    Adding validation to the budget field.

--- src/components/Budget.js
+++ src/components/Budget.js
@@ -2,14 +2,24 @@ import React, { useContext, useState } from 'react';
 import { AppContext } from '../context/AppContext';
 
 const Budget = () => {
+    const { expenses  } = useContext(AppContext);
     const { budget } = useContext(AppContext);
     const [newBudget, setNewBudget] = useState(budget);
     const handleBudgetChange = (event) => {
-        setNewBudget(event.target.value);
+        const totalExpenses = expenses.reduce((prevValue, currValue) => {
+            return prevValue + currValue.cost;
+        }, 0);
+        if (event.target.value < totalExpenses) {
+            alert("You cannot reduce the budget value lower than the spending");
+        } else if (event.target.value > 20000) {
+            alert("You cannot exceed a budget of 20000");
+        } else {
+            setNewBudget(event.target.value);
+        }
     }
     return (
 <div className='alert alert-secondary'>
-<span>Budget: £{budget}</span>
+<span>Budget: £</span>
 <input type="number" step="10" value={newBudget} onChange={handleBudgetChange}></input>
 </div>
     );
