9e1512eedc911db9a449b6b1e94915cd690469a5
Rupam Kuehner <rupamkuehner@gmail.com>
2024-04-05 18:31:02 -0400

    Reflecting the currency selection in the other fields and styling and adding increase and decrease by 10 buttons to Allocation table

--- src/components/Budget.js
+++ src/components/Budget.js
@@ -2,29 +2,32 @@ import React, { useContext, useState } from 'react';
 import { AppContext } from '../context/AppContext';
 
 const Budget = () => {
-    //const { dispatch } = useContext(AppContext);
+    const { dispatch } = useContext(AppContext);
     const { expenses  } = useContext(AppContext);
     const { budget } = useContext(AppContext);
+    const { currency } = useContext(AppContext);
     const [newBudget, setNewBudget] = useState(budget);
+
     const handleBudgetChange = (event) => {
         const totalExpenses = expenses.reduce((prevValue, currValue) => {
             return prevValue + currValue.cost;
         }, 0);
+        //const budgetValue = event.target.value;
         if (event.target.value < totalExpenses) {
             alert("You cannot reduce the budget value lower than the spending");
         } else if (event.target.value > 20000) {
             alert("You cannot exceed a budget of 20000");
         } else {
             setNewBudget(event.target.value);
-            /*dispatch({
+            dispatch({
                 type: 'SET_BUDGET',
-                payload: newBudget,
-            });*/
+                payload: event.target.value,
+            });
         }
     }
     return (
 <div className='alert alert-secondary'>
-<span>Budget: £</span>
+<span>Budget: {currency}</span>
 <input type="number" step="10" value={newBudget} onChange={handleBudgetChange}></input>
 </div>
     );
--- src/components/Currency.js
+++ src/components/Currency.js
@@ -1,14 +1,15 @@
-import React, { useContext, useState } from 'react';
+import React, { useContext } from 'react';
 import { AppContext } from '../context/AppContext';
 
 const Currency = () => {
-    //const { expenses  } = useContext(AppContext);
-    //const { budget } = useContext(AppContext);
-    const { currency } = useContext(AppContext);
-    const [newCurrency, setNewCurrency] = useState(currency);
+    const { dispatch } = useContext(AppContext);   
+
     
     const handleCurrencyChange = (event) => {
-        setNewCurrency(event.target.value);
+        dispatch({
+                type: 'CHG_CURRENCY',
+                payload: event.target.value,
+            });
     }
     
     /*
@@ -27,7 +28,7 @@ const Currency = () => {
 		<div className='alert alert-secondary'>
         <label style={{marginLeft: '1rem' , backgroundColor:'#90EE90', color:'white'}} >Currency
       <select name='hover_color'id="currency" onChange={handleCurrencyChange} style={{ marginLeft: '1rem' , backgroundColor:'#90EE90', color:'white', border: 'none'}}>
-        <option style={{color:'black'}} value="£">£ Pound</option>
+        <option selected="selected" style={{color:'black'}} value="£">£ Pound</option>
         <option style={{color:'black'}} value="$">$ Dollar</option>
         <option style={{color:'black'}} value="€">€ Euro</option>
         <option style={{color:'black'}} value="₹">₹ Rupee</option>
